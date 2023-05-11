c62e832a614dfa8a6029c230aad3a605363aa21d
Ashraful Haque <vselfnet@gmail.com>
2023-05-11 15:51:16 +0200

    Show Data with Specific id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config() // for dot .env security
 const port = process.env.PORT || 5000
 
@@ -24,6 +24,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+    const serviceCollection = client.db('carDoctorDB').collection('services')
+
+    // get data from mongo to server
+    // http://localhost:5000/services/
+    app.get('/services', async (req, res) =>{
+        const result = await serviceCollection.find().toArray();
+        res.send(result);
+    })
+
+    // checkout for specific id 
+    // [set route - loaders: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)]
+    // http://localhost:5000/services/645cd63525ee0fc71bcf2b73
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const options = {
+            projection: {title:1, price:1, service_id:1}
+        };
+        const result = await serviceCollection.findOne(query, options);
+        res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Car Doctor Server successfully connected to MongoDB!");
   } finally {
